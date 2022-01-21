9ffc1517b6fd0fa9f46eb438e1b415f1c8bab57d
CarlosCapriel <carlos_acaprielg@hotmail.com>
2022-01-20 22:31:07 -0600

    practice with flex on tailwind css

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,5 +3,12 @@
     <h1 class="bg-cream-100 text-cream-400 font-rale font-thin text-2xl">
       This is an about page
     </h1>
+    <div class="container mx-auto mt-5 text-center">
+      <div class="bg-gray-400 h-64 flex flex-wrap justify-between items-center">
+        <div class="bg-yellow-400 p-4 m-2">1</div>
+        <div class="bg-yellow-500 p-4 m-2 self-end">2</div>
+        <div class="bg-yellow-600 p-4 m-2">3</div>
+      </div>
+    </div>
   </div>
 </template>
